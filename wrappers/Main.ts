935e6ee586d100e80b2176d127c41af172fe8961
Alexey Ostrovsky <a.ostrovsky@pixelplex.io>
2024-10-24 13:52:28 +0300

    finishing wrapper

--- wrappers/Main.ts
+++ wrappers/Main.ts
@@ -1,6 +1,5 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode, storeStateInit } from '@ton/core';
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode, Slice, storeStateInit } from '@ton/core';
 
-//run npx bluprint build (jetton-wallet.fc) at first
 import walletHex from "../build/JettonWallet.compiled.json";
 
 const JETTON_WALLET_CODE = Cell.fromBoc(Buffer.from(walletHex.hex, 'hex'))[0];
@@ -39,5 +38,40 @@ export class Main implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async sendChangeAdmin(provider: ContractProvider, via: Sender, value: bigint, query_id: number, new_admin: Address) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body:   beginCell()
+                        .storeUint(0x2da38aaf, 32)
+                        .storeUint(query_id, 64)
+                        .storeAddress(new_admin)
+                    .endCell()
+        });
+    }
+
+    async sendWithdraw(provider: ContractProvider, via: Sender, value: bigint, query_id: number, reciever: Address, jetton_amount: number) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body:   beginCell()
+                        .storeUint(0x41836980, 32)
+                        .storeUint(query_id, 64)
+                        .storeCoins(jetton_amount)
+                        .storeAddress(reciever)
+                        .storeUint(0, 8)
+                    .endCell()
+        });
+    }
+
+    async getCurrentState(provider: ContractProvider) : Promise<[Address, number, Address]> {
+        const result = await provider.get('get_current_n_value', []);
+
+        let admin_addr: Address = result.stack.readAddress();
+        let commission_prescent: number = result.stack.readNumber();
+        let commission_addr: Address = result.stack.readAddress();
+        return [ admin_addr, commission_prescent, commission_addr ];
     }
 }
